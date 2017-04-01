6bda27dc13fa94e76c7686c265806be9c57fd1bd
Monica <a.miss.story@gmail.com>
2017-04-01 00:29:49 -0700

    last

--- app/assets/javascripts/stack.js
+++ app/assets/javascripts/stack.js
@@ -1,10 +1,32 @@
 $(document).on('turbolinks:load', function() {
 	var count = 0; 
+	var flipped_slides = [];
+
+	$('.flashcard-container .flashcard').click(function(event){
+		console.log("I'm fired too");
+		var card_id = "#" + $(this).attr('id') + "_top";
+
+
+		var slide_id = null;
+
+		$(card_id).attr('class').split(/\s+/).forEach(function(n){
+  			if(n.match(/slide_[0-9]+/)){ 
+      			slide_id = n;  
+      			return false;
+   			}
+		})
+
+		// First check if the slide is already in the array. If it isn't, add it.
+		// IF it is, don't add it. 
+		flipped_slides.push(slide_id);
+
+	
+	});
 
 	$('.left-control').click(function(){
 		
 		var prev_count = count - 1; 
-		
+
 
 		if(count > 0)
 		{
@@ -12,16 +34,19 @@ $(document).on('turbolinks:load', function() {
 			$('.slide_' + count).addClass('animated slideOutLeft');
 
 			setTimeout(function(){
+
+				if($('.slide_' + count).closest('.face').hasClass('front-flip'))
+					$('.slide_' + count).closest('.face').toggleClass('front-flip');
 			
 				$('.slide_' + count).fadeOut(0).toggleClass('selected');
 			
-				$('.slide_' + prev_count).fadeIn(90);
+				$('.slide_' + prev_count).fadeIn(250);
 
 				$('.slide_' + prev_count).toggleClass('selected');
 
 				$('.slide_' + count).removeClass('animated slideOutLeft');
-
 				
+
 				count =  prev_count;  	
 			
 
@@ -39,10 +64,13 @@ $(document).on('turbolinks:load', function() {
 			$('.slide_' + count).addClass('animated slideOutRight');
 
 			setTimeout(function(){
-			
+
+				if($('.slide_' + count).closest('.face').hasClass('front-flip'))
+					$('.slide_' + count).closest('.face').toggleClass('front-flip');
+
 				$('.slide_' + count).fadeOut(0).toggleClass('selected');
 			
-				$('.slide_' + next_count).fadeIn(90);
+				$('.slide_' + next_count).fadeIn(250);
 
 				$('.slide_' + next_count).toggleClass('selected');
 
